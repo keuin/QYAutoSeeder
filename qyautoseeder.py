751d37f48bd90b56d44187047814183444e44674
Keuin <keuinx@qq.com>
2019-03-08 20:45:59 +0800

    create firstly

---
+++ qyautoseeder.py
@@ -0,0 +1,9 @@
+# QingYing Auto Seeder
+# Create:  Keuin
+# Version: 1.0 alpha - only for test purpose
+
+import json
+
+
+with open('seeder_settings.py','r') as f:
+    config = json.loads(f.read())
